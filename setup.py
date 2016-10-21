c7c9291484b62f2a2dfd5238989d498b09a5261c
wrobell <wrobell@pld-linux.org>
2016-10-21 01:09:23 +0100

    - updated copyright header

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #
 # BTZen - Bluetooh Smart sensor reading library.
 #
-# Copyright (C) 2015 by Artur Wroblewski <wrobell@pld-linux.org>
+# Copyright (C) 2015-2016 by Artur Wroblewski <wrobell@pld-linux.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
