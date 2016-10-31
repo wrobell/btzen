7c87e26416b35e0e3c0fa2b57fe5213c78506373
wrobell <wrobell@pld-linux.org>
2016-10-31 17:45:28 +0000

    - renamed libraries dir to components

---
+++ btzen/error.py
@@ -0,0 +1,40 @@
+#
+# BTZen - Bluetooh Smart sensor reading library.
+#
+# Copyright (C) 2015-2016 by Artur Wroblewski <wrobell@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+class Error(Exception):
+    """
+    Base, abstract BTZen exception.
+    """
+
+class ConnectionError(Error):
+    """
+    Connection error.
+    """
+
+class ConfigurationError(Error):
+    """
+    Sensor configuration error.
+    """
+
+class DataReadError(Error):
+    """
+    Sensor data reading error.
+    """
+
+# vim: sw=4:et:ai
