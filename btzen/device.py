1c7029cc91cf9cac3345d996e2c33e27c13a1bee
wrobell <wrobell@riseup.net>
2020-02-10 23:50:14 +0000

    - update copyright header to include year 2020

--- btzen/device.py
+++ btzen/device.py
@@ -1,7 +1,7 @@
 #
 # BTZen - library to asynchronously access Bluetooth devices.
 #
-# Copyright (C) 2015-2019 by Artur Wroblewski <wrobell@riseup.net>
+# Copyright (C) 2015-2020 by Artur Wroblewski <wrobell@riseup.net>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
